248f897a54ad253eb78fad734c426db7bee65809
Manu214609 <150542974+Manu214609@users.noreply.github.com>
2024-03-05 17:39:34 +0000

    ok

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-//const sum = (a,b) => {
-   // return a + b
-//}
-//console.log(sum(7,3))
-//module.exports = { sum };
-
-// Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
     let valueInDollar = valueInEuro * 1.07;
--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-//const { sum } = require('./app.js');
-
-//test('adds 14 + 9 to equal 23', () => {
-    //let total = sum(14,9);
-   // expect(total).toBe(23);
-
-//});
-
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js');
